1b3baa7365b77b0ec209257a050056b8a6d51ff9
pratik radia <cutewidjack@gmail.com>
2021-08-01 18:37:07 +0530

    created git repo for 4corner gradient in c#

---
+++ gradient_panel/4cornergradientpanel.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Drawing;
+using System.Drawing.Drawing2D;
+using System.Windows.Forms;
+
+namespace gradient_panel
+{
+    public class _4cornergradientpanel : Panel //we can make four corner gradient using path gradient
+    {
+        public Color GTopLeft { get; set; } = Color.Transparent;//times 4 for 4 corner
+        public Color GTopRight { get; set; } = Color.Transparent;
+        public Color GBottomLeft { get; set; } = Color.Transparent;
+        public Color GBottomRight { get; set; } = Color.Transparent;//to avoid front color foreground color mixxing with actual gradient.
+
+        protected override void OnPaint(PaintEventArgs e)
+        {
+            base.OnPaint(e);
+            var colorArray = new Color[] { GTopLeft, GTopRight, GBottomRight, GBottomLeft };//array in circular form
+            GraphicsPath graphicsPath = new GraphicsPath();
+            graphicsPath.AddRectangle(this.ClientRectangle);
+
+            Graphics graphics = e.Graphics;
+            using (PathGradientBrush pathGradientBrush = new PathGradientBrush(graphicsPath)
+            {
+                CenterColor = Color.FromArgb((int)colorArray.Average(a => a.R), (int)colorArray.Average(a => a.G), (int)colorArray.Average(a => a.B)),
+                SurroundColors = colorArray
+            })
+            {
+                graphics.FillPath(pathGradientBrush, graphicsPath);
+            }//cool huh!
+        }
+    }
+}
---
+++ gradient_panel/Form1.Designer.cs
@@ -0,0 +1,62 @@
+﻿namespace gradient_panel
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this._4cornergradientpanel1 = new gradient_panel._4cornergradientpanel();
+            this.SuspendLayout();
+            // 
+            // _4cornergradientpanel1
+            // 
+            this._4cornergradientpanel1.GBottomLeft = System.Drawing.Color.Blue;
+            this._4cornergradientpanel1.GBottomRight = System.Drawing.Color.FromArgb(((int)(((byte)(128)))), ((int)(((byte)(128)))), ((int)(((byte)(255)))));
+            this._4cornergradientpanel1.GTopLeft = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(255)))));
+            this._4cornergradientpanel1.GTopRight = System.Drawing.Color.Fuchsia;
+            this._4cornergradientpanel1.Location = new System.Drawing.Point(3, 12);
+            this._4cornergradientpanel1.Name = "_4cornergradientpanel1";
+            this._4cornergradientpanel1.Size = new System.Drawing.Size(649, 454);
+            this._4cornergradientpanel1.TabIndex = 0;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(892, 498);
+            this.Controls.Add(this._4cornergradientpanel1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private _4cornergradientpanel _4cornergradientpanel1;
+    }
+}
+
